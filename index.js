6d9880c67ad2605666314ac78c62091ee7e02d13
MiMonMin <qminh293@gmail.com>
2019-10-15 09:02:32 +0700

    New Upload
    Connect with API from cloud by MongoDB

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './Main_Screen/MH_Tab';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
